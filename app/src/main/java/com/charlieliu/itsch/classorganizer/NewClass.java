56c38f5e66f2fa91c0582e7987f41d8074d4dc79
CharlieLiu <itscharlieliu@gmail.com>
2018-09-19 12:13:43 -0700

    Initial Commit

---
+++ app/src/main/java/com/charlieliu/itsch/classorganizer/NewClass.java
@@ -0,0 +1,13 @@
+package com.charlieliu.itsch.classorganizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewClass extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_class);
+    }
+}
